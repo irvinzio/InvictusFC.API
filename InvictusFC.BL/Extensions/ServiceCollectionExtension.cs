bd240bc9a14baad0fb6fa292e01cde9e615edbd8
irvinzio <irvinzio.ram@gmail.com>
2021-01-09 12:21:10 -0600

    add service extension

---
+++ InvictusFC.BL/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvictusFC.Service.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddRepositoryMappings(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Mapping.DataModelMappingProfile));
+            return services;
+        }
+        public static IServiceCollection RegisterServices( this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            return services;
+        }
+    }
+}
---
+++ InvictusFC.BL/Mapping/DataModelMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using InvictusFC.Data.Entities;
+using InvictusFC.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvictusFC.Service.Mapping
+{
+    public class DataModelMappingProfile :  Profile
+    {
+        public DataModelMappingProfile()
+        {
+            CreateMap<ClientRequest, User>().ReverseMap();
+            CreateMap<ClientResponse, User>().ReverseMap();
+        }
+    }
+}
