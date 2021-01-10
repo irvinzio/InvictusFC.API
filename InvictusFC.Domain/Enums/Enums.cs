312a13e45041edeb827a00d3a7934bdbb2fb4bdf
irvinzio <irvinzio.ram@gmail.com>
2021-01-10 04:48:52 -0600

    add weekdays enum

--- InvictusFC.Domain/Enums/Enums.cs
+++ InvictusFC.Domain/Enums/Enums.cs
@@ -1,36 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace InvictusFC.Domain.Enums
+﻿namespace InvictusFC.Domain.Enums
 {
-        public enum RolEnum
-        {
-            Admin,
-            Manager
-        }
-        public enum UserTypeEnum
-        {
-            Client,
-            Prospect,
-            Coach
-        }
-        public enum IdentificationEum
-        {
-            INE,
-            IFE,
-            Passport,
-            DrivingLicence
-        }
-        public enum CountryEnum
-        {
-            Mexico
-        }
-        public enum StateEnum
-        {
-            Guadalajara,
-            Zapopan,
-            Tlajomulco,
-            Tonala
-        }
+    public enum RolEnum
+    {
+        Admin,
+        Manager
+    }
+    public enum UserTypeEnum
+    {
+        Client,
+        Prospect,
+        Coach
+    }
+    public enum IdentificationEum
+    {
+        INE,
+        IFE,
+        Passport,
+        DrivingLicence
+    }
+    public enum CountryEnum
+    {
+        Mexico
+    }
+    public enum StateEnum
+    {
+        Guadalajara,
+        Zapopan,
+        Tlajomulco,
+        Tonala
+    }
+    public enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
 }
